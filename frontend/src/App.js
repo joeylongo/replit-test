abb77b323201887611e7077ff881f06099c3a75f
Adam Monica [C] <admonica@coca-cola.com>
2025-05-15 21:46:07 -0400

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SalesforceRecordView from './SalesforceRecordView';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SalesforceRecordView />
+      </header>
+    </div>
+  );
+}
+
+export default App;

aeeeadcc6e15f59e6adf482f530968455ca13876
Adam Monica [C] <admonica@coca-cola.com>
2025-05-30 11:56:35 -0400

    Updates to analyze context

--- scripts/analyze.ts
+++ scripts/analyze.ts
@@ -67,20 +67,31 @@ export async function appendCsvLine(
   await appendFile(filePath, line, 'utf8');
 }
 
+
+  // - Do not repeat information. Try to detect if an acronym or term is restating the same concept as another portion of the Execution Details.
+  //   For example: "12-pack Core CAN" means 12 pack of cans which are always 355ml. So adding: SSD: 12x355ml is confusing and repetetive.
+
+//   ** Please refer to these examples of excellent Execution Details **
+// - Execute: 6pk Half Liters at $Buy 2 Get 2 Free!!! (Incl. Core 5 +Add'| Flavors) Lobby and/or Primary Perimeter Display w/ Share A Coke POS. (MSC)(BBFC)
+// - Execute: 20pk Cans at $9.99 Weekly Digital Deal!!! (Incl. Core 5 +Add'! Flavors) Perimeter Display w/ Share A Coke POS. (MSC)(BBFC)
+// - Execute: Both BA Multi-Packs and PA 20oz 8pks at $4.99 Weekly Digital on Dry Grocery Endcap #5. (BB/so)
+// - Sell: Incremental Gold Peak 59oz Singles Perimeter Display at Net $2.49! {Buy 5 Save $5 MEGA}!! (Add KO Mini Cans, Half Liters, etc where applicable) Thru 5/27
+
 const go = async () => {
   let csv = ''
   let i = 0
   let start = false
   for (const sample of samples) {
-    if(sample.Id === 'a15a6000001ilRRAAY') {
-      start = true
-      continue
-    }
-    if(!start) {
-      console.log('skipping',sample.Id)
-      continue
-    }
+    // if(sample.Id === 'a15a6000001ilRRAAY') {
+    //   start = true
+    //   continue
+    // }
+    // if(!start) {
+    //   console.log('skipping',sample.Id)
+    //   continue
+    // }
     i++
+    if(i > 20) break
     const otherSystemPrompt = `You are an AI agent with knowledge about how a beverage company relays promotion execution strategy to front line sales.
 The company has promotions which involve setting up special product displays in-store.
 You are in the execution enablement group which is responsible for keeping track of the promotions and articulating to front-line sales people about how to set up the displays.
@@ -88,6 +99,7 @@ This groups creates a PicOS (Picture of Success) record in salesforce that track
 Each PicOS has a number of activities that each relate to a specific store, a number of their doors, and specific products/displays.
 
 Here is the guide on what makes a good activity:
+
 **Action Item Rules of Engagement **
 Our associates should have no more than 10-12 Action Items at a time
 If you have activity spanning multiple months, a new AI should be submitted for each month of execution
@@ -123,8 +135,8 @@ Tips for improving existing Execution Details:
   - Sometimes the person who originally wrote the Execution Details will include extra information that is not present in any data the Salesforce Record.
     If you identify words and phrases like this, then you should try to include them in the new rewritten Execution Details despite these terms not existing anywher else.
     The reason is that someone probably had some special knowledge not captured anywhere so that should be retained.
-  - Try to prefer easy to understand layman's terminology over acronyms and jargon within reason. For example: SSD: 12x355ml is more confusing than 12-pack Core CAN display.
-  - Do not repeat information. Try to detect if an acronym or term is restating the same concept as another portion of the Execution Details. For example you don't need to write: "12-pack Core CAN display (SSD: 12x355ml)" as this is repetive. Space is at a premium.
+  - Try to prefer easy to understand layman's terminology over acronyms and jargon within reason. 
+      For example: "SSD: 12x355ml" is more confusing than "12-pack Core CAN display". Simply write "12-pack Core CAN display" and omit "SSD: 12x355ml"
   - Certain words that exist only in the execution details should be weighted higher for inclusion in the newly rewritten Execution details.
     This includes words like: Shipper, pallet drop, storage bin. You are allowed to also come up with other words to weight highly if you think they belong in the same category as the given words.
 
@@ -146,6 +158,19 @@ Pillar Programs:
   - FF: Fall Football
   - Hol: Holiday
 
+**Guide to providing layman's terms and jargon to avoid**
+Use the following guide to use layman's terms over acronymns and jargon. Do NOT use the following terms:
+- SSD: sparkling soft drink. You dont need to include SSD because people will know which brands are SSDs.
+- 12 x 355ml: You shouldn't simply copy the package types info directly. You must instead say "12 pack of Cans" since cans are always 355ml.
+Here are some specific examples and how to improve them.
+1) Repetetive
+    - Bad example: "Execute 12-pack Core CAN display featuring SSD Core CAN 12oz x 355ml (12 pack) at $4.99. Deploy shelf talkers to the front of store/lobby. Utilize Simple Promo: 1 can for $4.99."
+    - Better example: "Execute 12-pack Core CAN display at $4.99. Deploy shelf talkers to the front of store/lobby. Utilize Simple Promo: 1 can for $4.99."
+2) Also repetetive
+    - Bad example: "Execute 12-pack Core CAN display at the front of store/lobby featuring SSD Core CAN 12oz/355ml 12pk. Implement shelf talkers with the $4.99 Simple Promo: 1 can for $4.99."
+    - Good example: "Execute 12-pack Core CAN display at the front of store/lobby. Implement shelf talkers with the $4.99 Simple Promo: 1 can for $4.99."
+
+**Your job**
 Your job is to focus on what good "Execution Details" look like. Execution Details is another term for the Action Item verbiage or "execution direction" referred to in the guide above.
 The "“PicOS” Look of Success" section of the guide covers good execution details.
 The Execution Details should refer to the Action Item Naming Conventions in the guide where applicable. For example, if the activity is "Market Street Challenge" = Yes, then put "MSC" somwhere to track it.
@@ -163,7 +188,7 @@ ${jsonToPlainText(sample)}
 Here is the Execution Details (Action Item verbiage) I want you to evaluate:
 ${sample.Product_Price_Execution_Direction__c}
 
-Please responsd with ONLY a re-written Execution Details (improvedExecutionDetails) to get it as high quality as possible given the information in the salesforce record.
+Please respond with ONLY a re-written Execution Details (improvedExecutionDetails) to get it as high quality as possible given the information in the salesforce record.
 Do not include any other commentary or reasoning.
 `;
     console.log(
@@ -196,6 +221,7 @@ Do not include any other commentary or reasoning.
 
     const original = stripHtmlTags(sample.Product_Price_Execution_Direction__c)
     const generated = stripHtmlTags(JSON.parse(res.message.content).improvedExecutionDetails)
+
     const link = `https://ccnag.lightning.force.com/lightning/r/Activation__c/${sample.Id}/view`
     const line = `${sample.Activity_Name__c},${original.replace(',', ';')},${generated.replace(',', ';')},${link}\n`
     console.log("Generated Execution Details:");
